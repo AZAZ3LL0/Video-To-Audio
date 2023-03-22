02cc6921140fc0c01eb14f98f6eb563a4deab5d8
Lolaperez <lolaperez1331@gmail.com>
2023-03-22 03:12:44 +0300

    Manual download added

---
+++ manual_download.py
@@ -0,0 +1,39 @@
+from pytube import YouTube
+import os
+from moviepy.editor import *
+import argparse
+from tools import titles
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--path", required=False)
+parser.add_argument("-s", "--stream", required=False)
+args = parser.parse_args()
+
+print(titles.intro2)
+
+
+def main():
+    path = args.path
+    while True:
+        request = input("YT link:\t")
+        if request == "exit" or request == KeyboardInterrupt:
+            break
+
+        yt = YouTube(request)
+
+        print(yt.title)
+
+        yt.streams.filter(file_extension='mp4')[0].download()
+        video = VideoFileClip(yt.title + ".mp4")
+        video.audio.write_audiofile(yt.title + ".mp3")
+
+        video.close()
+        os.remove(yt.title + ".mp4")
+
+        if args.stream is None:
+            break
+
+
+if __name__ == "__main__":
+    main()
