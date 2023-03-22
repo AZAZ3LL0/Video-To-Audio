3d583d5aa9d1fc74259d76f7416da1284cbacd32
Lolaperez <lolaperez1331@gmail.com>
2023-03-22 14:19:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from pytube import YouTube
 import os
 from moviepy.editor import *
@@ -10,14 +11,16 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, "Put youtube link")
+    bot.send_message(message.chat.id, "/download 'LINK'")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     yt = YouTube(message.text)
-    vidos = yt.title.replace('.', '')
+
+    vidos = "".join(char for char in yt.title if char.isalnum())
     print(vidos)
+
     yt.streams.filter(file_extension='mp4')[0].download()
     video = VideoFileClip(vidos + ".mp4")
     video.audio.write_audiofile(vidos + ".mp3")
@@ -30,8 +33,7 @@ def echo_all(message):
     audio.close()
     os.remove(vidos + ".mp4")
     os.remove(vidos + ".mp3")
-    #except BaseException:
-    #    bot.reply_to(message, "Еблан? Ссылку гони сучара")
+
 
 @bot.message_handler(commands=['download'])
 def sent_group_audio(message):
@@ -50,5 +52,6 @@ def sent_group_audio(message):
     audio.close()
     os.remove(vidos + ".mp4")
     os.remove(vidos + ".mp3")
+
 
 bot.infinity_polling(interval=0, timeout=5 * 60)
